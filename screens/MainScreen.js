d44d3ee74d1306751e23341ba4365a45a6a581c9
jeonga1226 <shinja1226@gmail.com>
2024-07-25 09:29:49 +0900

    KAN-38 chore : redux, hooks, screens 폴더 추가

---
+++ screens/MainScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+const MainScreen = () => {
+  return <View></View>;
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
