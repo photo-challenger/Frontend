fc992892ae5e460787e7f992a6784585cc8d2215
choijimin <jiminchoi1221@gmail.com>
2024-09-06 00:37:01 +0900

    KAN-71 feat: 홈 퍼블리싱, 공공 api 연결

---
+++ screens/MainRegionTabScreen.js
@@ -0,0 +1,361 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { FlatList, StatusBar, StyleSheet, KeyboardAvoidingView, Text, View, Image, ActivityIndicator, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import { useFocusEffect } from '@react-navigation/native';
+import Animated from 'react-native-reanimated';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { fetchAreaBasedList } from '../service/api';
+
+const Tab = createMaterialTopTabNavigator();
+
+const RegionItemComopnent = React.memo(({ navigation, item }) => {
+  const [clickBookmark, setClickBookmark] = useState(false);
+
+  const handleBookmarkClick = () => {
+    setClickBookmark(!clickBookmark);
+  };
+
+  const moveDetail = (id) => {
+    navigation.navigate('홈', { contentId: id });
+  };
+
+  return (
+    <RegionItemContainer activeOpacity={0.6} onPress={() => moveDetail(item.contentid)}>
+      {item.firstimage === '' ? (<RegionImage source={require('../assets/tripture-no-content.png')} />) : (<RegionImage source={{ uri: item.firstimage }} />)}
+      <RegionTextContainer>
+        <View style={{ maxWidth: '90%' }}>
+          <RegionName numberOfLines={1} ellipsizeMode="tail">{item.title}</RegionName>
+          <RegionAddress numberOfLines={1} ellipsizeMode="tail">{item.addr1}</RegionAddress>
+        </View>
+        <TouchableOpacity onPress={handleBookmarkClick}>
+          {clickBookmark ? (<BookmarkImage source={require('../assets/select-content-bookmark.png')} />) : (<BookmarkImage source={require('../assets/content-bookmark.png')} />)}
+        </TouchableOpacity>
+      </RegionTextContainer>
+    </RegionItemContainer>
+  )
+});
+
+const RegionComponent = ({ route, navigation }) => {
+  const [regionContentList, setRegionContentList] = useState([]);
+  const [startLoading, setStartLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [page, setPage] = useState(2);
+  const [totalPage, setTotalPage] = useState(0);
+  const { region } = route.params;
+
+  useFocusEffect(
+    useCallback(() => {
+      if (hasLoaded) return;
+
+      const getRegionContent = async () => {
+        try {
+          setStartLoading(true);
+          const apiResponseData = await fetchAreaBasedList(region, 1);
+          setRegionContentList(apiResponseData.items.item);
+          setTotalPage(Math.ceil(apiResponseData.totalCount / apiResponseData.numOfRows));
+        } catch (error) {
+          console.error('Failed to fetch region content:', error);
+        } finally {
+          setStartLoading(false);
+          setLoading(false);
+          setHasLoaded(true);
+        }
+      };
+
+      getRegionContent();
+    }, [region, hasLoaded])
+  );
+
+  const getData = async () => {
+    try {
+      if (page <= totalPage) {
+        setLoading(true);
+        const apiResponseData = await fetchAreaBasedList(region, page);
+        setRegionContentList(prevList => [...prevList, ...apiResponseData.items.item]);
+        setPage(prevPage => prevPage + 1);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error('Failed to fetch region content:', error);
+    }
+  }
+
+  const onEndReached = () => {
+    if (!loading) {
+      getData();
+    }
+  }
+
+  if (startLoading) {
+    return (
+      <LoadingContainer>
+        <ActivityIndicator size="large" color="#CA7FFE" />
+      </LoadingContainer>
+    );
+  }
+
+  return (
+    <RegionContainer>
+      <FlatList
+        data={regionContentList}
+        renderItem={({ item }) => <RegionItemComopnent item={item} navigation={navigation} />}
+        keyExtractor={(item) => item.contentid}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.6}
+        style={styles.flatListStyle}
+        ListFooterComponent={loading && (
+          <View style={{ padding: 10 }}>
+            <ActivityIndicator color="#CA7FFE" />
+          </View>
+        )}
+      />
+    </RegionContainer>
+  );
+}
+
+const CustomTabBar = ({ state, descriptors, navigation }) => {
+  return (
+    <TabListContainer>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ flexDirection: 'row' }}
+      >
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label = options.title !== undefined ? options.title : route.name;
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            navigation.navigate(route.name);
+          };
+
+          return (
+            <TabListFirstView
+              key={index}
+              onPress={onPress}
+              isFocused={isFocused}
+              activeOpacity={0.5}
+            >
+              <TabListFirstText isFocused={isFocused}>{label}</TabListFirstText>
+            </TabListFirstView>
+          );
+        })}
+      </ScrollView>
+    </TabListContainer>
+  );
+};
+
+const MainRegionTabScreen = ({ route, navigation }) => {
+  const RegionList = ['전체', '인천', '경기', '서울', '강원', '대전', '세종', '충청남도', '충청북도',
+    '광주', '전라남도', '전북', '부산', '울산', '경상남도', '대구', '경상북도', '제주'];
+
+  const initialRegionList = {
+    "서울": "서울",
+    "인천 경기": "인천",
+    "강원": "강원",
+    "충청": "대전",
+    "호남": "광주",
+    "영남": "부산",
+    "제주": "제주"
+  }
+
+  const { initialRegion } = route.params || { initialRegion: '전체' };
+  const paramsRegion = initialRegionList[initialRegion] || '전체';
+
+  const onSearchSubmit = (searchText) => {
+    // 검색 결과를 보여줄 새로운 화면으로 이동
+    navigation.navigate('MainSearchScreen', { query: searchText });
+  };
+
+  return (
+    <MainContainer>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingView}
+      >
+        <StatusBar barStyle="light-content" backgroundColor="#CA7FFE" />
+        <SearchContainer>
+          <SearchContent>
+            <SearchInput
+              placeholder="어떤 여행지를 찾으세요?"
+              onSubmitEditing={(e) => onSearchSubmit(e.nativeEvent.text)}
+              placeholderTextColor={"#C4C7CE"}
+            />
+            <SearchIconImg source={require('../assets/icon-search-home.png')} />
+          </SearchContent>
+          <TouchableOpacity>
+            <MapIconImg source={require('../assets/map-01.png')} />
+          </TouchableOpacity>
+        </SearchContainer>
+        <RegionTouristHeaderText>지역별 관광지</RegionTouristHeaderText>
+        {/* tab List */}
+        <Tab.Navigator
+          initialRouteName={paramsRegion}
+          tabBar={props => <CustomTabBar {...props} />}
+          screenOptions={{
+            tabBarIndicatorStyle: {
+              backgroundColor: '#000000', // 선택된 탭 아래의 Indicator 색상
+              height: 3, // Indicator 높이
+            },
+          }}>
+          {RegionList.map((region) => (
+            <Tab.Screen key={region} name={region} component={RegionComponent} initialParams={{ region }} />
+          ))}
+        </Tab.Navigator>
+
+      </KeyboardAvoidingView>
+    </MainContainer>
+  );
+};
+
+export default MainRegionTabScreen;
+
+const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  animatedSheet: {
+    flex: 1,
+  },
+  scrollView: {
+    flexGrow: 1, // Changed from flex: 1
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  flatListStyle: {
+    padding: 24,
+  }
+});
+
+const LoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TabListContainer = styled.View`
+	display: flex;
+	flex-direction: row;
+	margin-top: 16px;
+  padding-left: 12px;
+  padding-right: 12px;
+  border-bottom-width: 1px;
+  border-bottom-color: #CBCBCB;
+`
+
+const TabListFirstView = styled.TouchableOpacity`
+	margin-left: 12px;
+  margin-right: 12px;
+	border-bottom-width: 2px;
+	padding-bottom: 11px;
+	border-bottom-color: ${(props) =>
+    props.isFocused ? '#121212' : 'transparent'};
+`
+
+const TabListFirstText = styled.Text`
+	font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+	color: #121212;
+`
+
+const MainContainer = styled.View`
+  background: #F7F7F8;
+  height: 100%;
+`;
+
+const SearchContainer = styled.View`
+  background-color: #CA7FFE;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px 16px;
+  align-items: center;
+`
+
+const SearchContent = styled.View`
+  display: flex;
+  height: 52px;
+  border-radius: 25px;
+  background: #FFFFFF;
+  flex-direction: row;
+  padding: 8px 18px;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MapIconImg = styled.Image`
+  width: 30px;
+  height: 30px;
+  margin-left: 11px;
+`
+
+const SearchIconImg = styled.Image`
+  width: 22px;
+  height: 22px;
+  margin-left: 8px;
+`;
+
+const SearchInput = styled.TextInput`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+const RegionTouristHeaderText = styled.Text`
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  margin: 15px 0 18px 24px;
+`
+
+const RegionContainer = styled.View`
+  flex: 1;
+`
+
+const RegionItemContainer = styled.TouchableOpacity`
+  width: 100%;
+  height: 208px;
+  padding: 16px;
+  background-color: #FFFFFF;
+  border-radius: 12px;
+  margin-bottom: 12px;
+`;
+
+const RegionTextContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const RegionImage = styled.Image`
+  height: 128px;
+  width: 100%;
+  border-radius: 8px;
+`;
+
+const RegionName = styled.Text`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  margin-bottom: 4px;
+  margin-top: 8px;
+`;
+
+const RegionAddress = styled.Text`
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  color: #A6A6A6;
+`;
+
+const BookmarkImage = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
+
---
+++ screens/MainSearchScreen.js
@@ -0,0 +1,250 @@
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar, StyleSheet, KeyboardAvoidingView, Text, View, Image, ActivityIndicator, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import Animated from 'react-native-reanimated';
+import { fetchSearchKeyword } from '../service/api';
+
+const SearchItemComponent = ({ navigation, item }) => {
+
+	const moveDetail = (id) => {
+    navigation.navigate('홈', { contentId: id });
+  };
+
+	return (
+		<SearchResultItem activeOpacity={0.6} onPress={() => moveDetail(item.contentid)}>
+			<SearchResultItemDetail>
+				{item.addr1 === '' ? (null) : (
+					<>
+					<SearchResultRegion>{item.addr1.split(' ')[0]}</SearchResultRegion>
+					<View></View>
+					</>
+				)}
+				<View>
+					<SearchResultName numberOfLines={1} ellipsizeMode="tail">{item.title}</SearchResultName>
+					{item.addr1 === '' ? (null) : (<SearchResultAddress numberOfLines={1} ellipsizeMode="tail">{item.addr1}</SearchResultAddress>)}
+				</View>
+			</SearchResultItemDetail>
+			{item.firstimage === '' ? (<SearchResultImage source={require('../assets/tripture-no-content.png')} />) : (<SearchResultImage source={{ uri: item.firstimage }} />)}
+		</SearchResultItem>
+	)
+}
+
+const MainSearchScreen = ({ route, navigation }) => {
+	const { query } = route.params;
+	const [searchResult, setSearchResult] = useState();
+	const [searchKeyword, setSearchKeyword] = useState(query);
+	const [loading, setLoading] = useState(true);
+	const [startLoading, setStartLoading] = useState(true);
+	const [totalPage, setTotalPage] = useState(0);
+	const [totalCount, setTotalCount] = useState(0);
+	const [page, setPage] = useState(2);
+
+	useEffect(() => {
+		getSearchResult();
+	}, []);
+
+	const getSearchResult = async () => {
+		try {
+			setStartLoading(true);
+			const apiResponseData = await fetchSearchKeyword(searchKeyword, 1);
+			setSearchResult(apiResponseData.items.item);
+			setTotalPage(Math.ceil(apiResponseData.totalCount / apiResponseData.numOfRows));
+			setTotalCount(apiResponseData.totalCount);
+		} catch (error) {
+			console.error('Failed to fetch region detail content:', error);
+		} finally {
+			setStartLoading(false);
+			setLoading(false);
+		}
+	};
+
+	const getData = async () => {
+		try {
+			if (page <= totalPage) {
+				setLoading(true);
+				const apiResponseData = await fetchSearchKeyword(searchKeyword, page);
+				setSearchResult(prevList => [...prevList, ...apiResponseData.items.item]);
+				setPage(prevPage => prevPage + 1);
+				setLoading(false);
+			}
+		} catch (error) {
+			console.error('Failed to fetch region content:', error);
+		}
+	}
+
+	const onEndReached = () => {
+		if (!loading) {
+			getData();
+		}
+	}
+
+	if (startLoading) {
+		return (
+			<LoadingContainer>
+				<ActivityIndicator size="large" color="#CA7FFE" />
+			</LoadingContainer>
+		);
+	}
+
+	const handleSearchSubmit = () => {
+		setPage(2);
+		setSearchResult([]);
+		getSearchResult();
+	};
+
+	return (
+		<MainSearchContainer>
+			<SearchContainer>
+				<TouchableOpacity>
+					<ChevronLeftImage source={require('../assets/btn-back.png')} />
+				</TouchableOpacity>
+				<SearchContent>
+					<SearchInput
+						value={searchKeyword}
+						placeholder="어떤 여행지를 찾으세요?"
+						onChangeText={(text) => setSearchKeyword(text)}
+						placeholderTextColor={"#C4C7CE"}
+						onSubmitEditing={() => handleSearchSubmit()}
+					/>
+				</SearchContent>
+			</SearchContainer>
+			<SearchHeaderText><Text style={{ color: "#CA7FFE" }}>{totalCount}개</Text>의 장소를 찾았어요.</SearchHeaderText>
+			{searchResult.length !== 0 ? (
+				<FlatList
+					data={searchResult}
+					renderItem={({ item }) => <SearchItemComponent item={item} navigation={navigation} />}
+					keyExtractor={(item) => item.contentid}
+					onEndReached={onEndReached}
+					onEndReachedThreshold={0.6}
+					style={styles.flatListStyle}
+					ListFooterComponent={loading && (
+						<View style={{ padding: 10 }}>
+							<ActivityIndicator color="#CA7FFE" />
+						</View>
+					)}
+				/>
+			) : (
+				<LoadingContainer>
+					<Text style={{ color: "#858585" }}>검색 결과가 존재하지 않습니다.</Text>
+				</LoadingContainer>
+			)}
+		</MainSearchContainer>
+	);
+};
+
+export default MainSearchScreen;
+
+const styles = StyleSheet.create({
+	keyboardAvoidingView: {
+		flex: 1,
+	},
+	animatedSheet: {
+		maxHeight: '100%',
+	},
+	scrollView: {
+		flexGrow: 1, // Changed from flex: 1
+	},
+	scrollViewContent: {
+		flexGrow: 1,
+	}
+});
+
+const LoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainSearchContainer = styled.View`
+  background: #F7F7F8;
+  height: 100%;
+`;
+
+const SearchContainer = styled.View`
+	display: flex;
+	flex-direction: row;
+	margin: 30px 14px 0 14px;
+	align-items: center;
+`
+
+const ChevronLeftImage = styled.Image`
+	width: 32px;
+	height: 32px;
+`
+
+const SearchContent = styled.View`
+  display: flex;
+  height: 42px;
+  border-radius: 30px;
+  background: #FFFFFF;
+  border: 1.1px solid #666666;
+	justify-content: center;
+	padding: 8px 24px;
+	background: #F7F7F8;
+	flex: 1;
+`;
+
+const SearchInput = styled.TextInput`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+const SearchHeaderText = styled.Text`
+	font-size: 14px;
+	font-style: normal;
+	font-weight: 400;
+	color: #858585;
+	margin-left: 24px;
+	margin-top: 18px;
+	margin-bottom: 8px;
+`
+
+const SearchResultItem = styled.TouchableOpacity`
+	display: flex;
+	flex-direction: row;
+	background-color: #FFFFFF;
+	justify-content: space-between;
+	padding: 20px 24px;
+	border-bottom-width: 1px;
+	border-bottom-color: #A6A6A6;
+`
+
+const SearchResultItemDetail = styled.View`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 70%;
+`
+
+const SearchResultName = styled.Text`
+	font-size: 18px;
+	font-style: normal;
+	font-weight: 700;
+`
+
+const SearchResultRegion = styled.Text`
+	font-size: 13px;
+	font-style: normal;
+	font-weight: 600;
+	color: #FFFFFF;
+	background-color: #B5B5B5;
+	padding: 6px 14px;
+	border-radius: 50px;
+	margin-left: -2px;
+	position: absolute;
+`
+
+const SearchResultAddress = styled.Text`
+	font-size: 14px;
+	font-style: normal;
+	font-weight: 400;
+	color: #A6A6A6;
+`
+
+const SearchResultImage = styled.Image`
+	width: 85px;
+	height: 85px;
+	border-radius: 8px;
+	margin-left: 15px;
+`
