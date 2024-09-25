7d817fde38001ae7e582bbe9ebb07e06762fdd10
choijimin <jiminchoi1221@gmail.com>
2024-09-25 20:43:38 +0900

    KAN-209 feat: 커뮤니티 페이징 추가

--- screens/CommunityScreen.js
+++ screens/CommunityScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, TouchableOpacity, StatusBar, Dimensions } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StatusBar, Dimensions, ActivityIndicator, FlatList } from 'react-native';
 import styled from 'styled-components';
 import {
   fetchPopularCommunityList,
@@ -9,34 +9,101 @@ import {
 const { width } = Dimensions.get('window');
 const itemWidth = width / 3 - 22.5;
 
-const CommunityScreen = ({ route, navigation }) => {
-  const [text, onChangeText] = useState('');
-  const [challList, setchallList] = useState([]);
-
-  useEffect(() => {
-    getCommunityList();
-  }, []);
-
-  async function getCommunityList() {
-    const apiResponseData = await fetchPopularCommunityList({ page: 0 });
-    console.log(' apiResponseData   :', apiResponseData);
-    setchallList(apiResponseData?.popularCalculateDtoList);
-  }
-
-  async function searchRegion() {
-    console.log('text  : ', text);
-    const apiResponseData = await fetchSearchCommnunityRegion({
-      searchOne: text,
-    });
-
-    console.log(' apiResponseData   :', apiResponseData);
-
-    setchallList(apiResponseData?.searchResponseList);
-  }
+const CommunityImageContainer = ({ item, navigation }) => {
 
   const moveDetail = (id) => {
     navigation.navigate('communityDetail', { postId: id });
   };
+
+  return (
+    <TouchableOpacity
+      onPress={() => moveDetail(item.postId)}
+    >
+      <ImageItem source={{ uri: item.postImgName }} />
+    </TouchableOpacity>
+  )
+}
+
+const CommunityScreen = ({ route, navigation }) => {
+  const [text, onChangeText] = useState('');
+  const [challList, setchallList] = useState([]);
+  const [startLoading, setStartLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+
+  useEffect(() => {
+    if (text === '') {
+      handleSearchSubmit();
+    }
+  }, [text]);
+
+  useEffect(() => {
+    if (page === 0 && totalPage === 0 && challList.length === 0) {
+      getData();
+    }
+  }, [page]);
+
+  async function searchRegion() {
+    handleSearchSubmit();
+  }
+
+  function handleSearchSubmit() {
+    setPage(0);
+    setTotalPage(0);
+    setchallList([]);
+  };
+
+  const getData = async () => {
+    try {
+      if (page <= totalPage) {
+        if (text !== "") {
+          if (page === 0) {
+            setStartLoading(true);
+            const apiResponseData = await fetchSearchCommnunityRegion({
+              searchOne: text,
+              page: page,
+            });
+            setchallList(apiResponseData?.searchResponseList);
+            setTotalPage(apiResponseData?.totalPages - 1);
+          } else {
+            setLoading(true);
+            const apiResponseData = await fetchSearchCommnunityRegion({
+              searchOne: text,
+              page: page,
+            });
+            setchallList(prevList => [...prevList, ...apiResponseData?.searchResponseList]);
+          }
+
+          setPage(prevPage => prevPage + 1);
+        } else {
+          if (page === 0) {
+            setStartLoading(true);
+            const apiResponseData = await fetchPopularCommunityList(page);
+            setchallList(apiResponseData?.popularCalculateDtoList);
+            setTotalPage(apiResponseData?.totalPages - 1);
+          } else {
+            setLoading(true);
+            const apiResponseData = await fetchPopularCommunityList(page);
+            setchallList(prevList => [...prevList, ...apiResponseData?.popularCalculateDtoList]);
+          }
+
+          setPage(prevPage => prevPage + 1);
+        }
+      }
+    } catch (error) {
+      console.error('Failed to fetch region content:', error);
+    } finally {
+      setLoading(false);
+      setStartLoading(false);
+    }
+  }
+
+  const onEndReached = () => {
+    if (!loading) {
+      getData();
+    }
+  }
 
   return (
     <ListContainer>
@@ -47,25 +114,49 @@ const CommunityScreen = ({ route, navigation }) => {
           placeholder="어떤 여행지를 찾으세요?"
           onChangeText={(text) => onChangeText(text)}
           onSubmitEditing={searchRegion}
+          value={text}
         />
       </SearchContent>
 
-      <ImageListContainer>
-        {challList &&
-          challList.map((item) => (
-            <TouchableOpacity
-              key={item.postId}
-              onPress={() => moveDetail(item.postId)}
-            >
-              <ImageItem source={{ uri: item.postImgName }} />
-            </TouchableOpacity>
-          ))}
-      </ImageListContainer>
+      {startLoading ? (
+        <LoadingContainer>
+          <ActivityIndicator size="large" color="#CA7FFE" />
+        </LoadingContainer>
+      ) : (
+        Array.isArray(challList) && challList.length != 0 ? (
+            <ImageListContainer>
+              <FlatList
+                data={challList}
+                renderItem={({ item }) => <CommunityImageContainer item={item} navigation={navigation} />}
+                keyExtractor={(item) => item.postId.toString()}
+                onEndReached={onEndReached}
+                onEndReachedThreshold={0.6}
+                numColumns={3}
+                ListFooterComponent={loading && (
+                  <View style={{ padding: 10 }}>
+                    <ActivityIndicator color="#CA7FFE" />
+                  </View>
+                )}
+              />
+            </ImageListContainer>
+          ) : (
+            <LoadingContainer>
+              <Text style={{ color: "#858585", fontFamily: "Medium", fontSize: 15 }}>검색 결과가 존재하지 않습니다.</Text>
+            </LoadingContainer>
+          )
+        )}
     </ListContainer>
   );
 };
 
 export default CommunityScreen;
+
+const LoadingContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
 
 const ListContainer = styled.View`
   display: flex;
